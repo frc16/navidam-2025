05023d8fb439a45ce7c3221b8dda9e68cd96bcf5
Samu7526 <samueljosue312005@gmail.com>
2025-12-18 10:44:29 +0100

    Melodia Evangelion - Samuel

---
+++ backend/sdk/src/main/java/com/navidam/sdk/api/melodia/Evangelion.java
@@ -0,0 +1,30 @@
+package com.navidam.sdk.api.melodia;
+
+import com.navidam.sdk.internal.melodia.*;
+
+public class Evangelion {
+
+	public static Melodia crearMelodia() {
+		
+	        return new Melodia("Evangelion")
+	            .instrumento(Instrumento.FLAUTA_DULCE)
+	            .tempo(120)
+	            .nota(Nota.SOL, Figura.NEGRA)
+	            .nota(Nota.SOL, Figura.NEGRA)
+	            .nota(Nota.LA, Figura.NEGRA)
+	            .nota(Nota.SOL, Figura.BLANCA)
+	        	.nota(Nota.RE, Figura.NEGRA)
+	        	.nota(Nota.MI, Figura.NEGRA)
+	        	.nota(Nota.FA, Figura.NEGRA)
+	        	.nota(Nota.MI, Figura.BLANCA)
+	        	.nota(Nota.LA, Figura.NEGRA)
+	        	.nota(Nota.LA, Figura.NEGRA)
+	        	.nota(Nota.SOL, Figura.NEGRA)
+	        	.nota(Nota.FA, Figura.BLANCA);
+	}
+	
+	public static void main(String[] args) {
+		Melodia melodia = crearMelodia();
+		System.out.printf("Melodia 1: %s%n",melodia);
+	}
+}
