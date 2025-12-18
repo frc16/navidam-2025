3f48c24e5cb0fae5eee604bb84e6f4168dfcde32
Sergio <sanvitero2000@gmail.com>
2025-12-18 09:53:40 +0100

    alta

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,7 +1,9 @@
 import {createBrowserRouter} from "react-router-dom";
 import PageNotFound from "@/pages/PageNotFound.tsx";
+//import AltaPersona from "@/pages/AltaPersona";
 
 
 export const router = createBrowserRouter([
     {path: "*", element: <PageNotFound/>},
+    //{path: "/altapersona", element: <AltaPersona/>},
 ]);
