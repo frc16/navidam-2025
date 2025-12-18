7c28098600e04d06ef9a31cbf9d1e3031b9b7fd2
Mikel <soymikelnevado@gmail.com>
2025-12-18 10:27:53 +0100

    melodia jingle bells

---
+++ backend/sdk/src/main/java/com/navidam/sdk/api/melodias/jingleBell.java
@@ -0,0 +1,23 @@
+package com.navidam.sdk.api.melodias;
+import com.navidam.sdk.internal.melodia.*;
+public class jingleBell {
+
+
+		public static Melodia crear() {
+			
+			return new Melodia("Jingle Bell")
+				.instrumento(Instrumento.PIANO_ACUSTICO)
+				.tempo(131)
+				.nota(Nota.SOL, Figura.NEGRA)
+				.nota(Nota.DO, Figura.NEGRA)
+				.nota(Nota.SI, Figura.NEGRA)
+				.nota(Nota.DO, Figura.NEGRA)
+				.nota(Nota.LA, Figura.NEGRA);
+				
+		}
+		public static void Main(String[] args) throws Exception{
+			Melodia m = jingleBell.crear();
+					
+			m.escuchar();
+		}
+}
