094d06110d5fe70b011d2a941b1bb090b8e11a2c
Mokel400 <soymikelnevado@gmail.com>
2025-12-18 12:31:28 +0100

    Update Meme.java

--- backend/sdk/src/main/java/com/navidam/sdk/api/meme/Meme.java
+++ backend/sdk/src/main/java/com/navidam/sdk/api/meme/Meme.java
@@ -10,11 +10,4 @@ public class Meme {
 			    "Vacaciones de Navidad",
 			    "Exámenes en enero"
 			).width(800).url();
-	public static void main(String[] args) {
-		String url = Memegen.of(MemeTemplate.GRU.id(), "Feliz navidad", "y que apruebes todas", "o si no...",
-				"nos vemos el curso que viene").width(800).url();
-
-		System.out.println(url);
-
-	}
 }
