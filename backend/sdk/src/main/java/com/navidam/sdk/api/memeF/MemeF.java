fb73979be5842944ab2c925d496b020e0cd48143
frc16 <153406170+frc16@users.noreply.github.com>
2025-12-18 12:13:06 +0100

    memef

---
+++ backend/sdk/src/main/java/com/navidam/sdk/api/memeF/MemeF.java
@@ -0,0 +1,20 @@
+package com.navidam.sdk.api.memeF;
+
+import com.navidam.sdk.internal.meme.*;
+
+public class MemeF {
+
+   
+String url = Memegen.of(
+        MemeTemplate.CAPTAIN_AMERICA.id(),
+    
+        "celebras navidad?",
+        "soy ateo",
+        "entonces te voy hacer conocer al yiyus"
+).width(800).url();
+
+    }
+    
+
+
+
